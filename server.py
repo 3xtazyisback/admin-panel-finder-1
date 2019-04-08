80f2e204bff93d481bd2c1493ab77de52d2fd573
Ahmet Ümit BAYRAM <38078662+ahmetumitbayram@users.noreply.github.com>
2019-04-08 09:59:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -44,4 +44,4 @@ while True:
                     print(son_liste.index(i)+1,i, "bağlantı yok")
                     continue
             finally:
-                dosya.close()
+                    dosya.close()
